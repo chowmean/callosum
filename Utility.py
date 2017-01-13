0902cb7c32052a95a4e51a1b053df298c0e63a8c
@chowmean [Gaurav Yadav] <gaurav.dev.iiitm@gmail.com>
2017-01-14 04:21:42 +0530

    commit

---
+++ Utility.py
@@ -0,0 +1,77 @@
+import boto
+import boto.s3.connection
+import csv
+import os
+
+class awsS3:
+	def getfile(self,access_key,secret_key,database):
+		print "Getting file from s3"
+		conn = boto.connect_s3(
+	        aws_access_key_id = access_key,
+	        aws_secret_access_key = secret_key,
+	        is_secure=False
+	        )
+		bucket = conn.get_bucket("prod-practo-tipoca")
+		keys_list=[]
+		marker = None
+		while True:
+			print "next itter"
+			keys = bucket.get_all_keys(prefix="bckp_"+database,marker=marker)
+			last_key = None
+
+			for k in keys:
+			    keys_list.append(k)
+			    last_key = k.name
+
+			if not keys.is_truncated:
+			    break
+			marker = last_key
+		return keys_list
+
+	def splitFile(self,filename):
+		divisor=10000
+		outfile=None
+		outfileno = 1
+		with open(filename, 'r') as infile:
+		    for index, row in enumerate(csv.reader(infile)):
+		        if index % divisor == 0:
+		            if outfile is not None:
+		                outfile.close()
+		            if not os.path.exists('split_file'):
+		    			os.makedirs('split_file')
+		            outfilename = 'split_file/'+filename.split('.')[0]+'__big-{}.csv'.format(outfileno)
+		            outfile = open(outfilename, 'w')
+		            outfileno += 1
+		            writer = csv.writer(outfile)
+		        writer.writerow(row)
+
+ 	def createQuery(self):
+ 		a=open('create.sql','r')
+		b=open('created.sql','w')
+		statement=""
+
+		with open("create.sql") as f:
+		    arr=[word for line in f for word in line.split()]
+		    inp=False
+		    for word in arr:
+		    	if word == "CREATE":
+		    		inp=True
+		    	if(inp==True):
+		    		statement=statement+word
+		    		statement=statement+" "
+		    	if(word==");"):
+		    	 	inp=False
+		statement=statement.replace('character varying','varchar')
+		statement=statement.replace('character','varchar')
+		statement=statement.replace('timestamp without time zone','datetime')
+		b.write(statement);
+		a.close()
+		b.close()
+		return statement
+
+	def deleteAllTable(self,tablesArray):
+		string=""
+		with open("delete.sql",'w') as f:
+			for table in tablesArray:
+				string=string+"drop table " + table + ";";
+			f.write(string)
---
+++ run.py
@@ -0,0 +1,43 @@
+import boto.rds
+import mysql.connector
+import csv
+import sys
+import Utility
+import os
+import time
+from ProjectConfig import *
+from os import listdir, rmdir, remove
+from subprocess import call,Popen
+
+def do_work(database):
+    if(database=="feedback"):
+        fileset = FEEDBACK_TABLES
+    else:
+        fileset = FABRIC_TABLES
+    a = Utility.awsS3()
+    access_key = AWSKEY
+    secret_key = AWSSECRET
+    starttime=time.time()
+    filelist=a.getfile(access_key,secret_key,database)
+    filelist = sorted(filelist, key=lambda k: k.last_modified)
+    filelist.reverse()
+    Iterated=[]
+    for fileitem in fileset:
+    	for file in filelist:
+    		if  file.name.split('/')[1].split('$')[0]==fileitem and fileitem not in Iterated:
+    			Iterated.append(fileitem)
+    			print file.name
+    			file.get_contents_to_filename(file.name.split('/')[1].split('$')[0]+'.csv')
+    			temp_process1=Popen('mysqlimport --local -u '+LOCAL_USER+' -p'+LOCAL_PASSWORD+' --fields-terminated-by=, '+database+' '+file.name.split('/')[1].split('$')[0]+'.csv', shell=True)
+    			temp_process1.wait()
+	                remove_csv_process=Popen('rm '+file.name.split('/')[1].split('$')[0]+'.csv', shell=True)
+        	        remove_csv_process.wait()
+               		temp_process2=Popen('mysqldump -u '+LOCAL_USER+' -p'+LOCAL_PASSWORD+' '+database+' '+ file.name.split('/')[1].split('$')[0] +' > table.sql', shell=True)
+    	       		temp_process2.wait()
+           		temp_process3=Popen('mysql -u '+DBUSER+' -h '+HOST+' -p'+DBPASSWORD+' '+database+' < table.sql', shell=True)
+           		temp_process3.wait()
+
+
+if __name__ == "__main__":
+    do_work('feedback')
+    do_work('fabric')
