04d2e57d0e4dc23914762af340de531da8e24e46
@chowmean [Gaurav Yadav] <gaurav.dev.iiitm@gmail.com>
2017-01-17 14:43:01 +0530

    finishing up

--- fabfile.py
+++ fabfile.py
@@ -10,32 +10,10 @@ from ProjectConfig import *
 # Set the username
 env.user   = "ubuntu"
 
-def list_file():
-    """
-        Update the default OS installation's
-        basic default tools.
-                                            """
-    run("ls -l")
 
 def get_project():
     run("sudo apt-get install -y git")
     run("git clone https://github.com/chowmean/callosum.git")
-
-def install_pip():
-    run("wget https://pypi.python.org/packages/11/b6/abcb525026a4be042b486df43905d6893fb04f05aac21c32c638e939e447/pip-9.0.1.tar.gz#md5=35f01da33009719497f01a4ba69d63c9")
-    run("tar -xvzf pip-9.0.1.tar.gz")
-    run("cd pip-9.0.1")
-    run("python setup.py install")
-
-def install_dependencies():
-    run("pip install boto")
-    run("sudo apt-get install mysql-server")
-
-def make_installs():
-    run("aptitude   update")
-    run("aptitude -y upgrade")
-    install_pip()
-    get_project()
 
 def runn():
     # make_installs()
